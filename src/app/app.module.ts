8c6fb8d1a6261f77d93d238e9ea57ff26b5d7ebc
Louay Morsi <louay.hesham@gmail.com>
2017-12-19 14:58:27 +0200

    Added navbar and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
