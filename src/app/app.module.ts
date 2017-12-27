a3c1121c95007f1a421bd9be25979a65d18a4c13
Louay Morsi <louay.hesham@gmail.com>
2017-12-27 22:06:54 +0200

    Made disclaimer disappear when proceeding to the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { DisclaimerComponent } from './disclaimer/disclaimer.component';
+import { MainComponent } from './main/main.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    DisclaimerComponent
+    DisclaimerComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
