594d654fd6700b91e17dcc8262b559cd45db2d49
Louay Morsi <louay.hesham@gmail.com>
2018-01-02 18:32:47 +0200

    Split general and CCE courses into seperate files, also edited python script to write to the respective JSON file for each department

--- scripts/courses_JSON_generator.py
+++ scripts/courses_JSON_generator.py
@@ -4,51 +4,92 @@
 import json
 import sys
 
+general = { }
+department = { }
+
+def get_courses_info(courses):
+  level = 0
+  print("""Enter subject info in the following format:
+        [Course Code],[Name],[Prerequisites],[Credit hours]
+          Example: MP104,Mechanics 2,MP101&MP103,3
+        Enter: term=[h | e | 1..10] to specify which level are the following subjects, you can change them anytime.
+          h for humanity subjects, e for elective subjects, numbers from 1 to 10 represtents the terms.
+          Example: term=h, term=7 ... etc
+        Enter "done" to terminate""")
+
+  while(True):
+    subject_str = input('> ')
+    if subject_str == 'done':
+      break
+    if subject_str[0:4] == 'term':
+      level = subject_str.split('=')[1];
+      print("Now entering subjects in level " + level)
+    elif level == 0:
+      print("Please specify the term first")
+    else:
+      subject_info = subject_str.split(',')
+      new_course = {
+        'code': subject_info[0].upper(),
+        'name': subject_info[1].title(),
+        'prerequisites': [] if subject_info[2] == '' else subject_info[2].upper().split('&'),
+        'satisfies': [],
+        'credit_hours': int(subject_info[3]),
+        'level': level
+      }
+      courses[new_course['code']] = new_course
+      print(new_course)
+      print("\n")
+
+      for prerequisite in new_course['prerequisites']:
+        if prerequisite in courses:
+          courses[prerequisite]['satisfies'].append(new_course['code'])
+        else:
+          general[prerequisite]['satisfies'].append(new_course['code'])
+
+
 try:
-  with open('Courses.JSON') as data_file:
-    courses = json.load(data_file)
-    print('Loaded courses from JSON.')
+  with open('General.JSON') as data_file:
+    general = json.load(data_file)
+    print('Loaded general courses from JSON.')
 except (OSError, IOError, ValueError) as e:
-  print("File not found, re enter everything again.")
+  print("General courses file not found, re-enter the info again")
+  get_courses_info(general)
+  with open('General.JSON', 'w') as outfile:
+    json.dump(general, outfile)
 
-print(courses)
-level = 0
-while(True):
-  subject_str = input("""Enter subject info in the following format:
-      [Course Code],[Name],[Prerequisites],[Credit hours]
-        Example: MP204,Modern Physics,MP101&MP107,3
-      Enter: term=[h | e | 1..10] to specify which level are the following subjects, you can change them anytime.
-        h for humanity subjects, e for elective subjects, numbers from 1 to 10 represtents the terms.
-        Example: term=h, term=7 ... etc
-      Enter "null" to terminate
-      Course Info: """
-    )
-  if subject_str == 'null':
-    break
+n = input("""Choose the department:
+  1- Gas and Petrochemicals Engineering
+  2- Electromechanical Engineering
+  3- Architectural and Construction Engineering
+  4- Computer and Communications Engineering
+  5- Offshore and Coastal Engineering
+  6- General Department
 
-  if subject_str[0:4] == 'term':
-    level = subject_str.split('=')[1];
-    print("Now entering subjects in level " + level)
-  elif level == 0:
-    print("Please specify the level first")
-  else:
-    subject_info = subject_str.split(',')
-    new_course = {
-      'code': subject_info[0].upper(),
-      'name': subject_info[1].title(),
-      'prerequisites': [] if subject_info[2] == '' else subject_info[2].upper().split('&'),
-      'satisfies': [],
-      'credit_hours': int(subject_info[3]),
-      'level': level
-    }
-    courses[new_course['code']] = new_course
-    print(new_course)
-    print("\n")
+  Enter department number: 
+  """)
 
-    for prerequisite in new_course['prerequisites']:
-      courses[prerequisite]['satisfies'].append(new_course['code'])
+department_name = {
+  '1': 'GPE', 
+  '2': 'EME',
+  '3': 'CAE',
+  '4': 'CCE',
+  '5': 'OCE',
+  '6': 'General'
+}[n]
+file_name = department_name + '.JSON'
+if n == '6':
+  department = general
 
+try:
+  with open(file_name) as data_file:
+    department = json.load(data_file)
+    print('Loaded ' + department_name + ' courses from JSON.')
+except (OSError, IOError, ValueError) as e:
+  print(department_name + " courses file not found, re-enter the info again")
 
-with open('Courses.JSON', 'w') as outfile:
-  json.dump(courses, outfile)
+get_courses_info(department)
+with open(file_name, 'w') as outfile:
+  json.dump(department, outfile)
 
+with open('General.JSON', 'w') as outfile:
+  json.dump(general, outfile)
