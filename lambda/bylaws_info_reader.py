1b1a7d701ef8d0d46b0aa66dbe075b3a9c66398a
Louay Morsi <louay.hesham@gmail.com>
2020-10-22 01:43:57 +0200

    Added API Gateway to stack template

--- lambda/bylaws_info_reader.py
+++ lambda/bylaws_info_reader.py
@@ -1,7 +1,11 @@
           import boto3
+          import json
           def lambda_handler(event, context):
             dynamodb = boto3.resource('dynamodb')
             table = dynamodb.Table('Bylaws')
             ddb_scan_response = table.scan()
             bylaws = ddb_scan_response['Items']
-            return bylaws
+            return {
+                "statusCode": 200,
+                "body": json.dumps(bylaws)
+            }
